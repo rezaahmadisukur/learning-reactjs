be89e88548de386cd0636ca7e7583daa2d8e11e2
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2025-08-04 19:50:17 +0700

    First commit, install dependencies for new project app, kasir-app

---
+++ projects/kasir-app/kasir-app/src/App.js
@@ -0,0 +1,12 @@
+import NavbarComponent from "./components/NavbarComponent";
+
+function App() {
+    return (
+        <div className="App">
+            <NavbarComponent />
+            <h2>Halo</h2>
+        </div>
+    );
+}
+
+export default App;
