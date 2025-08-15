251924aa4c1c39fc2c32150c21aac65b84d2fed9
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2025-08-15 10:37:06 +0700

    Eps 06 Dynamic Component

---
+++ tutorials-react-js/prawito-hudoro/06-dynamic-component/src/components/Home/Home.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import YouTubeComp from "../YoutubeComp/YouTubeComp";
+
+export default class Home extends Component {
+    render() {
+        return (
+            <>
+                <h1>YouTube Component</h1>
+                <hr />
+                <div style={{ display: "flex" }}>
+                    <YouTubeComp
+                        time="07:12"
+                        title="Tutorial React JS - Part 1"
+                        desc="2x views 2 days ago"
+                    />
+                    <YouTubeComp
+                        time="05:10"
+                        title="Tutorial React JS - Part 2"
+                        desc="10x views 5 days ago"
+                    />
+                    <YouTubeComp
+                        time="09:04"
+                        title="Tutorial React JS - Part 3"
+                        desc="3x views 3 days ago"
+                    />
+                    <YouTubeComp
+                        time="10:10"
+                        title="Tutorial React JS - Part 4"
+                        desc="1x views 1 days ago"
+                    />
+                    <YouTubeComp />
+                </div>
+            </>
+        );
+    }
+}
