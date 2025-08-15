b1ea45892b54542d1b2e001a7409fbe741f0aa93
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2025-08-15 09:10:18 +0700

    Eps 05 StateFull and StateLess

---
+++ tutorials-react-js/prawito-hudoro/05-stateLess-stateFull/src/components/HelloComponent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./HelloComponent.css";
+
+const HelloComponent = () => {
+    return <h1>Hello Component</h1>;
+};
+
+export default HelloComponent;
---
+++ tutorials-react-js/prawito-hudoro/05-stateLess-stateFull/src/container/StateFullComponent.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class StateFullComponent extends Component {
+    render() {
+        return <div>StateFullComponent</div>;
+    }
+}
+
+export default StateFullComponent;
