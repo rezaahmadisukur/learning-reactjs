3a99f85ef744022a9a4cc9dcb7ed861a4d892618
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2025-08-17 20:33:04 +0700

    Rename Eps 8 to Eps 8 part 1 & 2

--- tutorials-react-js/prawito-hudoro/08-lifeCycle/src/container/StateFullComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class StateFullComponent extends Component {
-    render() {
-        return <div>StateFullComponent</div>;
-    }
-}
-
-export default StateFullComponent;
