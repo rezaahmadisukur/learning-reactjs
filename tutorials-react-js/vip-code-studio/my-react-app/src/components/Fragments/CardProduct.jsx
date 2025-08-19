efdcdb26aa9f25fbda8c3fe0c804f9d0dc950e0f
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2025-08-19 08:34:02 +0700

    Eps 09 Rendering List

--- tutorials-react-js/vip-code-studio/my-react-app/src/components/Fragments/CardProduct.jsx
+++ tutorials-react-js/vip-code-studio/my-react-app/src/components/Fragments/CardProduct.jsx
@@ -2,7 +2,7 @@ import Button from "../Elements/Button";
 
 const CardProduct = ({ children }) => {
     return (
-        <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow">
+        <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow flex flex-col justify-between ">
             {children}
         </div>
     );
@@ -17,12 +17,12 @@ const Header = (props) => {
     );
 };
 
-const Body = ({ children, title }) => {
+const Body = ({ children, name }) => {
     return (
-        <div className="px-5 pb-5">
+        <div className="px-5 pb-5 h-full">
             <a href="#">
                 <h5 className="text-xl font-semibold tracking-tight text-white">
-                    {title}
+                    {name}
                 </h5>
                 <p className="text-sm text-white">{children}</p>
             </a>
--- tutorials-react-js/vip-code-studio/my-react-app/src/pages/products.jsx
+++ tutorials-react-js/vip-code-studio/my-react-app/src/pages/products.jsx
@@ -1,28 +1,46 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
+const products = [
+    {
+        id: 1,
+        name: "Sepatu Baru",
+        image: "/images/shoes-1.jpg",
+        price: "500.000",
+        description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Eveniet exercitationem temporibus molestiae. Iure itaque
+        quis reiciendis hic dicta minus corporis fugiat sed cum et.
+        Eos dignissimos quaerat consequatur omnis voluptatem.`
+    },
+    {
+        id: 2,
+        name: "Sepatu Lama",
+        image: "/images/shoes-1.jpg",
+        price: "300.000",
+        description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Eveniet exercitationem temporibus molestiae.`
+    },
+    {
+        id: 3,
+        name: "Sepatu Nike",
+        image: "/images/shoes-1.jpg",
+        price: "1.000.000",
+        description: `Nike Sepatu Baru Terbaru 2023 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Eveniet exercitationem temporibus molestiae.`
+    }
+];
+
 const ProductPage = () => {
     return (
         <div className="flex justify-center py-5 gap-4">
-            <CardProduct>
-                <CardProduct.Header image="/images/shoes-1.jpg" />
-                <CardProduct.Body title="Sepatu Baru">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Eveniet exercitationem temporibus molestiae. Iure itaque
-                    quis reiciendis hic dicta minus corporis fugiat sed cum et.
-                    Eos dignissimos quaerat consequatur omnis voluptatem.
-                </CardProduct.Body>
-                <CardProduct.Footer price="500.000" />
-            </CardProduct>
-            <CardProduct>
-                <CardProduct.Header image="/images/shoes-1.jpg" />
-                <CardProduct.Body title="Sepatu Baru">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Eveniet exercitationem temporibus molestiae. Iure itaque
-                    quis reiciendis hic dicta minus corporis fugiat sed cum et.
-                    Eos dignissimos quaerat consequatur omnis voluptatem.
-                </CardProduct.Body>
-                <CardProduct.Footer price="500.000" />
-            </CardProduct>
+            {products.map((product) => (
+                <CardProduct key={product.id}>
+                    <CardProduct.Header image={product.image} />
+                    <CardProduct.Body name={product.name}>
+                        {product.description}
+                    </CardProduct.Body>
+                    <CardProduct.Footer price={product.price} />
+                </CardProduct>
+            ))}
         </div>
     );
 };
