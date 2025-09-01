1acb80e55a48736a4156dfa1dad1ab8fdafb3942
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2025-09-01 09:41:27 +0700

    Memo

--- tutorials-react-js/vip-code-studio/react-lanjutan/src/App.jsx
+++ tutorials-react-js/vip-code-studio/react-lanjutan/src/App.jsx
@@ -1,4 +1,4 @@
-import { useId } from "react";
+import { memo, useId, useState } from "react";
 import "./App.css";
 
 const Input = () => {
@@ -7,18 +7,53 @@ const Input = () => {
         <>
             <label htmlFor="">
                 <span>Name: </span>
-                <input type="text" id={inputId} />
+                <input type="text" id={`input-${inputId}`} />
             </label>
             <p>Name should contain at least firstname and lastname</p>
         </>
     );
 };
 
-function App() {
+const Todolist = memo((props) => {
+    const { todos } = props;
+    console.log("call todolist");
     return (
         <>
-            <Input />
-            <Input />
+            <h2>My Todos</h2>
+            {todos.map((todo, index) => (
+                <p key={`${todo}-${index}`}>{todo}</p>
+            ))}
+        </>
+    );
+});
+
+const Counter = memo((props) => {
+    const { count } = props;
+    console.log("call counter");
+    return (
+        <>
+            <p>Count: {count}</p>
+        </>
+    );
+});
+
+function App() {
+    const [count, setCount] = useState(0);
+    const [todos, setTodo] = useState(["Conding", "Create Video"]);
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const addTodo = (newTodos) => {
+        setTodo([...todos, newTodos]);
+    };
+    return (
+        <>
+            <Todolist todos={todos} />
+            <button onClick={() => addTodo("Playing Piano")}>Add Todo</button>
+            <Counter count={count} />
+            <button onClick={increment}>increment</button>
         </>
     );
 }
