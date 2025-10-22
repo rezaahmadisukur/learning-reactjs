1f2e871d5180495cdb03e1ab4202e8edc87607d0
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2025-10-22 20:21:12 +0700

    Make Login Page with loading and toaser

--- projects/projectmanagement/client/src/main.tsx
+++ projects/projectmanagement/client/src/main.tsx
@@ -3,14 +3,18 @@ import { createRoot } from "react-dom/client";
 // import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
-import App from "./App.tsx";
+import LoginPage from "./pages/login/LoginPage.tsx";
+import { Toaster } from "./components/ui/sonner.tsx";
+import SignupPage from "./pages/signup/SignupPage.tsx";
 
 const root = document.getElementById("root") as HTMLElement;
 
 createRoot(root).render(
   <BrowserRouter>
+    <Toaster duration={2000} position="top-right" />
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
       <Route path="/test" element={<h1>HELLO INI TEST</h1>} />
     </Routes>
   </BrowserRouter>
