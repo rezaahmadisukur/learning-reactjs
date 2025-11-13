55d47071689b712e3b243aed5d46dcfd542ecf16
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2025-11-13 07:05:04 +0700

    Belajar React FrontEnd JWT

---
+++ tutorials-react-js/m-fikri/jwt-auth/frontend/src/components/Register.tsx
@@ -0,0 +1,102 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPassword, setConfPassword] = useState("");
+  const [msg, setMsg] = useState("");
+  const navigate = useNavigate();
+
+  const handleRegister = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/users", {
+        name: name,
+        email: email,
+        password: password,
+        confPassword: confPassword
+      });
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+      if (axios.isAxiosError(error) && error.response) {
+        console.log(error.response.data.msg);
+        setMsg(error.response.data.msg);
+      }
+    }
+  };
+
+  return (
+    <section className="hero has-background-grey-light is-fullheight is-fullwidth">
+      <div className="hero-body">
+        <div className="container">
+          <div className="columns is-centered">
+            <div className="column is-4-desktop">
+              <form onSubmit={handleRegister} className="box">
+                <p className="has-text-centered">{msg}</p>
+                <div className="field mt-5">
+                  <label className="label">Name</label>
+                  <div className="controls">
+                    <input
+                      type="text"
+                      className="input"
+                      placeholder="John Doe"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="field mt-5">
+                  <label className="label">Email</label>
+                  <div className="controls">
+                    <input
+                      type="email"
+                      className="input"
+                      placeholder="johndoe@exmaple.ocm"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="field mt-5">
+                  <label className="label">Password</label>
+                  <div className="controls">
+                    <input
+                      type="password"
+                      className="input"
+                      placeholder="********"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="field mt-5">
+                  <label className="label">Confirm Password</label>
+                  <div className="controls">
+                    <input
+                      type="password"
+                      className="input"
+                      placeholder="********"
+                      value={confPassword}
+                      onChange={(e) => setConfPassword(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="field mt-5">
+                  <button className="button is-success is-fullwidth">
+                    Register
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
