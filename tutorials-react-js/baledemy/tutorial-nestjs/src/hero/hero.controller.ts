51af24ba824f54e11ccc0214f0cc1c3af8b4949d
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-01-01 16:55:09 +0700

    tutorial response object nestjs

--- tutorials-react-js/baledemy/tutorial-nestjs/src/hero/hero.controller.ts
+++ tutorials-react-js/baledemy/tutorial-nestjs/src/hero/hero.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Res } from '@nestjs/common';
+import type { Response } from 'express';
 
 @Controller('hero')
 export class HeroController {
   @Get('index') // hero/index
-  index(): string {
-    return 'hero index';
+  @HttpCode(200)
+  index(@Res() response: Response) {
+    return response.json({
+      title: 'hero world',
+    });
   }
 
   @Get('create') // hero/create
