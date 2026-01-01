2c7b8b339a2d7b116c407f7e53d7ccc271e8fa2d
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-01-01 21:04:02 +0700

    tutorial resource controller nestjs

--- tutorials-react-js/baledemy/tutorial-nestjs/src/app.module.ts
+++ tutorials-react-js/baledemy/tutorial-nestjs/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HeroController } from './hero/hero.controller';
+import { UserModule } from './user/user.module';
+import { RoleController } from './role/role.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController, HeroController],
+  imports: [UserModule],
+  controllers: [AppController, HeroController, RoleController],
   providers: [AppService],
 })
 export class AppModule {}
--- tutorials-react-js/baledemy/tutorial-nestjs/src/hero/hero.controller.ts
+++ tutorials-react-js/baledemy/tutorial-nestjs/src/hero/hero.controller.ts
@@ -1,19 +1,89 @@
-import { Controller, Get, HttpCode, Res } from '@nestjs/common';
-import type { Response } from 'express';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
+import type { Request, Response } from 'express';
+
+const heroes = [
+  {
+    id: 1,
+    nama: 'Aurora',
+    type: 'Mage',
+    gambar: 'aurora.jpg',
+  },
+  {
+    id: 2,
+    nama: 'Zilong',
+    type: 'Fighter',
+    gambar: 'zilong.jpg',
+  },
+  {
+    id: 3,
+    nama: 'Akai',
+    type: 'Tank',
+    gambar: 'akai.jpg',
+  },
+];
 
 @Controller('hero')
 export class HeroController {
   @Get('index') // hero/index
   @HttpCode(200)
   index(@Res() response: Response) {
-    return response.json({
-      title: 'hero world',
-    });
+    return response.json(heroes);
   }
 
   @Get('create') // hero/create
   create(@Res({ passthrough: true }) response: Response): string {
     response.cookie('name', 'baledemia');
     return 'hero create';
+  }
+
+  @Post('store')
+  store(
+    @Req() request: Request,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const { id, nama, type, gambar } = request.body as {
+      id: number;
+      nama: string;
+      type: string;
+      gambar: string;
+    };
+
+    heroes.push({
+      id,
+      nama,
+      type,
+      gambar,
+    });
+
+    return response.status(201).json({
+      message: 'Create successfully',
+      data: heroes,
+    });
+  }
+
+  @Delete('destroy')
+  destroy(
+    @Req() request: Request,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const { id } = request.body as {
+      id: number;
+    };
+
+    const deleted = heroes.filter((hero) => hero.id !== id);
+
+    return response.status(200).json({
+      message: 'Delete Successfully',
+      data: deleted,
+    });
   }
 }
