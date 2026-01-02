769a3533ea23335a8007b85520c6ddd7bf76bf98
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-01-02 09:49:22 +0700

    tutorial Modules nestjs

--- tutorials-react-js/baledemy/tutorial-nestjs/src/app.module.ts
+++ tutorials-react-js/baledemy/tutorial-nestjs/src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HeroController } from './hero/hero.controller';
 import { UserModule } from './user/user.module';
 import { RoleController } from './role/role.controller';
-import { HeroService } from './hero/hero.service';
 import { HeroModule } from './hero/hero.module';
 
 @Module({
   imports: [UserModule, HeroModule],
-  controllers: [AppController, HeroController, RoleController],
-  providers: [AppService, HeroService],
+  controllers: [AppController, RoleController],
+  providers: [AppService],
 })
 export class AppModule {}
