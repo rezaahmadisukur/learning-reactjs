f9b8d61e55f65d85ba665a70a691a2c0a4f2d12c
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-01-03 09:28:36 +0700

    tutorial jwt nestjs

--- tutorials-react-js/baledemy/tutorial-nestjs/src/auth/auth.module.ts
+++ tutorials-react-js/baledemy/tutorial-nestjs/src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [UserModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtService],
 })
 export class AuthModule {}
--- tutorials-react-js/baledemy/tutorial-nestjs/src/auth/auth.service.ts
+++ tutorials-react-js/baledemy/tutorial-nestjs/src/auth/auth.service.ts
@@ -2,10 +2,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
 import { UserService } from 'src/user/user.service';
 import { compare } from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
   async login(dto: AuthDto) {
     const userExists = await this.userService.findByEmail(dto.email);
 
@@ -19,12 +23,17 @@ export class AuthService {
       throw new UnauthorizedException('User or Password not match');
     }
 
-    const { email } = userExists;
+    const { id, email } = userExists;
+    const payload = { sub: id, username: email };
 
-    const user = {
-      user: email,
+    const userToken = {
+      access_token: await this.jwtService.signAsync(payload, {
+        secret:
+          'z5Y6VFgKtZPmpHD6jV9zRHur44YcHzO6DEptBM4Gd2hr1dIOshWx2I8zeO4c3+P0KnU=',
+        expiresIn: '60s',
+      }),
     };
 
-    return user;
+    return userToken;
   }
 }
