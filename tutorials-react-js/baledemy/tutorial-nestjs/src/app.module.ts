9eefdbe18896f38ce06bec6fd0c957dc6bbf8ecb
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-01-03 08:50:56 +0700

    tutorial authentication init nestjs

--- tutorials-react-js/baledemy/tutorial-nestjs/src/app.module.ts
+++ tutorials-react-js/baledemy/tutorial-nestjs/src/app.module.ts
@@ -6,6 +6,7 @@ import { RoleController } from './role/role.controller';
 import { HeroModule } from './hero/hero.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AuthModule,
   ],
   controllers: [AppController, RoleController],
   providers: [AppService],
