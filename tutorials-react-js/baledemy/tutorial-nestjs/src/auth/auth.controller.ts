a3aa45ed68df6efc998aba3967daad8e210686d1
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-01-03 09:14:59 +0700

    tutorial login with compare password nestjs

--- tutorials-react-js/baledemy/tutorial-nestjs/src/auth/auth.controller.ts
+++ tutorials-react-js/baledemy/tutorial-nestjs/src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthDto } from './dto/auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post('login')
+  login(@Body() authDto: AuthDto) {
+    return this.authService.login(authDto);
+  }
+}
--- tutorials-react-js/baledemy/tutorial-nestjs/src/auth/auth.service.ts
+++ tutorials-react-js/baledemy/tutorial-nestjs/src/auth/auth.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthDto } from './dto/auth.dto';
+import { UserService } from 'src/user/user.service';
+import { compare } from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+  async login(dto: AuthDto) {
+    const userExists = await this.userService.findByEmail(dto.email);
+
+    if (!userExists) {
+      throw new UnauthorizedException('User or Password not match');
+    }
+
+    const matchPassword = await compare(dto.password, userExists.password);
+
+    if (!matchPassword) {
+      throw new UnauthorizedException('User or Password not match');
+    }
+
+    const { email } = userExists;
+
+    const user = {
+      user: email,
+    };
+
+    return user;
+  }
+}
