8965d8c64aeef4419c87e9531848a6e0de79169c
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-01-18 08:51:20 +0700

    setup docker

---
+++ tutorials-react-js/haider-malik/nestjs-docker/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Make sure our backend app does not start before db ready
+echo "Waiting for database..."
+
+while ! nc -z db 5432; do
+  sleep 0.1 #short pause between checks
+done
+echo "Database started"
+
+# Apply db migrations
+npx prisma migrate deploy
+
+# Generate prisma client
+npx prisma generate
+
+# start the app
+exec "$@"
